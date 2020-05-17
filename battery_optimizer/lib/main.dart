6fe9da07e1a4c76fc5d9a9bdc8684d41719d0cbc
umatoma <umatomakun@gmail.com>
2020-05-17 12:51:08 +0900

    Battery Optimizer 作成

--- battery_optimizer/lib/main.dart
+++ battery_optimizer/lib/main.dart
@@ -1,117 +1,323 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+const kColorPurple = Color(0xFF8337EC);
+const kColorPink = Color(0xFFFF006F);
+const kColorIndicatorBegin = kColorPink;
+const kColorIndicatorEnd = kColorPurple;
+const kColorTitle = Color(0xFF616161);
+const kColorText = Color(0xFF9E9E9E);
+const kElevation = 4.0;
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.white,
+        primaryTextTheme: TextTheme(
+          headline6: TextStyle(color: kColorTitle),
+        ),
+        backgroundColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BatteryOptimizerPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+/* BatteryOptimizer画面 */
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class BatteryOptimizerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Battery Optimizer'),
+        centerTitle: false,
+        elevation: 0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            OptimizerButtons(),
+            BatteryLevelIndicator(),
+            AppsDrainage(),
+            OptimizeNow(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+/* Optimizer一覧関連 */
+
+class _OptimizerButton extends StatelessWidget {
+  final String text;
+
+  const _OptimizerButton({
+    Key key,
+    @required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Colors.white,
+      elevation: kElevation,
+      height: 24,
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      onPressed: () {},
+      child: Text(
+        text,
+        style: TextStyle(color: kColorTitle, fontSize: 12),
+      ),
+    );
+  }
+}
+
+class OptimizerButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            SizedBox(width: 16),
+            _OptimizerButton(text: 'Battery Optimizer'),
+            SizedBox(width: 16),
+            _OptimizerButton(text: 'Connection Optimizer'),
+            SizedBox(width: 16),
+            _OptimizerButton(text: 'Memory Optimizer'),
+            SizedBox(width: 16),
+            _OptimizerButton(text: 'Storage Optimizer'),
+            SizedBox(width: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/* バッテリーインジケーター関連 */
+
+class _BatteryLevelIndicatorPainter extends CustomPainter {
+  final double percentage; // バッテリーレベルの割合
+  final double textCircleRadius; // 内側に表示される白丸の半径
+
+  _BatteryLevelIndicatorPainter({
+    @required this.percentage,
+    @required this.textCircleRadius,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 1; i < (360 * percentage); i += 5) {
+      final per = i / 360.0;
+      // 割合（0~1.0）からグラデーション色に変換
+      final color = ColorTween(
+        begin: kColorIndicatorBegin,
+        end: kColorIndicatorEnd,
+      ).lerp(per);
+      final paint = Paint()
+        ..color = color
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 4;
+
+      final spaceLen = 16; // 円とゲージ間の長さ
+      final lineLen = 24; // ゲージの長さ
+      final angle = (2 * pi * per) - (pi / 2); // 0時方向から開始するため-90度ずらす
+
+      // 円の中心座標
+      final offset0 = Offset(size.width * 0.5, size.height * 0.5);
+      // 線の内側部分の座標
+      final offset1 = offset0.translate(
+        (textCircleRadius + spaceLen) * cos(angle),
+        (textCircleRadius + spaceLen) * sin(angle),
+      );
+      // 線の外側部分の座標
+      final offset2 = offset1.translate(
+        lineLen * cos(angle),
+        lineLen * sin(angle),
+      );
+
+      canvas.drawLine(offset1, offset2, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class BatteryLevelIndicator extends StatelessWidget {
+  final percentage = 0.7;
+  final size = 164.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _BatteryLevelIndicatorPainter(
+        percentage: percentage,
+        textCircleRadius: size * 0.5,
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(64),
+        child: Material(
+          color: Colors.white,
+          elevation: kElevation,
+          borderRadius: BorderRadius.circular(size * 0.5),
+          child: Container(
+            width: size,
+            height: size,
+            child: Center(
+              child: Text(
+                '${percentage * 100}%',
+                style: TextStyle(color: kColorPink, fontSize: 48),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/* アプリ一覧関連 */
+
+class _HorizontalBorder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1,
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      color: Colors.grey[200],
+    );
+  }
+}
+
+class _AppColumn extends StatelessWidget {
+  final String name;
+  final Icon icon;
+  final String percentage;
+
+  const _AppColumn({
+    Key key,
+    @required this.name,
+    @required this.icon,
+    @required this.percentage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: icon,
+      title: Text(
+        name,
+        style: TextStyle(color: kColorText),
+      ),
+      trailing: Text(
+        percentage,
+        style: TextStyle(color: kColorText),
+      ),
+    );
+  }
+}
+
+class AppsDrainage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: ClipOval(
+            child: Container(
+              color: kColorPurple,
+              child: Icon(Icons.apps, color: Colors.white),
+            ),
+          ),
+          title: Text(
+            'Apps Drainage',
+            style: TextStyle(color: kColorTitle),
+          ),
+          subtitle: Text(
+            'Show the most draining energy application',
+            style: TextStyle(color: kColorText),
+          ),
+        ),
+        Material(
+          color: Colors.white,
+          elevation: kElevation,
+          borderRadius: BorderRadius.circular(16),
+          child: Column(
+            children: [
+              _AppColumn(
+                icon: Icon(Icons.sms, color: Colors.indigo),
+                name: 'SMSApp',
+                percentage: '75%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.live_tv, color: Colors.red),
+                name: 'MovieApp',
+                percentage: '60%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.place, color: Colors.green),
+                name: 'MapApp',
+                percentage: '35%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.local_grocery_store, color: Colors.orange),
+                name: 'ShoppingApp',
+                percentage: '35%',
+              ),
+              _HorizontalBorder(),
+              _AppColumn(
+                icon: Icon(Icons.train, color: Colors.black),
+                name: 'TrainApp',
+                percentage: '15%',
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+/* 最適化ボタン関連 */
+
+class OptimizeNow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 24),
+      child: RaisedButton(
+        color: kColorPurple,
+        padding: EdgeInsets.symmetric(horizontal: 48),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {},
+        child: Text('Optimize Now', style: TextStyle(color: Colors.white)),
+      ),
     );
   }
 }
